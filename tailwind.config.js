d41a2193cfdd682499cfe63228d1264a157ed7c8
irhasalif <irhasalif@gmail.com>
2022-04-28 10:48:47 +0700

    :lipstick: Custom theme
    Menambah tema custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        'ungu' : '#24325F',
-        'birumuda': '#3E9CC1',
-        'birutua': '#3F9CBF',
-        'coklat': '#C0AF85',
-      },
+
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+            "primary": "#5F9758",
+            "secondary": "#C0AF85",
+            "accent": "#966745",
+            "neutral": "#000000",
+            "base-100": "#EAEAEA",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#F87272",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
   plugins: [
     require("@tailwindcss/typography"),
