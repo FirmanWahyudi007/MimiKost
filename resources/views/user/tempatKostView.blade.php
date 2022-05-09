c1f8f133efc903754ec75fd4530d8d4c04630e01
irhasalif <irhasalif@gmail.com>
2022-05-09 20:23:41 +0700

    :bug: z-index swiper dan jendela status
    sekarang jendela status posisi absolute

--- resources/views/user/tempatKostView.blade.php
+++ resources/views/user/tempatKostView.blade.php
@@ -12,54 +12,57 @@ Mimi Kost | Alamat Tempat Kost
 
 @section('body')
 <div class="container mx-auto ">
-    {{-- Swiper --}}
-    <div id="swiper" class="bg-blue-200 m-4 h-[30vh] lg:h-[40vh]">
-        <div class="bg-center bg-cover"
-            style="background-image: url(https://api.lorem.space/image/shoes);">
+    {{-- Swiper & Jendela status--}}
+    <div class="relative">
+        {{-- Swiper --}}
+
+        <div id="swiper" class="bg-blue-200 m-4 h-[30vh] lg:h-[40vh]">
+            <div class="bg-center bg-cover" style="background-image: url(https://api.lorem.space/image/shoes);">
+            </div>
+            <div class="bg-center bg-cover" style="background-image: url(https://api.lorem.space/image/shoes);">
+            </div>
+            <div class="bg-center bg-cover" style="background-image: url(https://api.lorem.space/image/shoes);">
+            </div>
         </div>
-        <div class="bg-center bg-cover"
-            style="background-image: url(https://api.lorem.space/image/shoes);">
-        </div>
-        <div class="bg-center bg-cover"
-            style="background-image: url(https://api.lorem.space/image/shoes);">
+
+        {{-- Jendela Status --}}
+        <div
+            class="m-4 grid grid-cols-1 gap-y-2 lg:bg-white lg:shadow-md lg:mx-auto lg:grid-cols-3 lg:p-4 lg:rounded-xl lg:w-[50vw] lg:absolute lg:-bottom-14 lg:right-0 lg:left-0">
+            <div class="flex gap-2 lg:flex-col">
+                <h2 class="hidden lg:block text-xl font-bold">Lokasi</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" class="lg:hidden h-5 w-5" viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                        clip-rule="evenodd" />
+                </svg>
+                <div class="">Lorem Ipsum</div>
+            </div>
+            <div class="flex gap-2 lg:flex-col">
+                <h2 class="hidden lg:block text-xl font-bold">Jumlah Kamar</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class="lg:hidden h-5 w-5"
+                    fill="currentColor" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 20">
+                    <path fill="currentColor"
+                        d="M20 9.557V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.525 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.475-.811-2.75-2-3.443zM18 7v2h-5V7h5zM6 7h5v2H6V7zm14 9H4v-3c0-1.103.897-2 2-2h12c1.103 0 2 .897 2 2v3z" />
+                </svg>
+                <div class="">Lorem Ipsum</div>
+            </div>
+            <div class="flex gap-2 lg:flex-col">
+                <h2 class="hidden lg:block text-xl font-bold">Sisa kamar</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class="lg:hidden h-5 w-5"
+                    preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 20">
+                    <path fill="currentColor"
+                        d="M20 9.556V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.526 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.474-.811-2.75-2-3.444zM11 9H6V7h5v2zm7 0h-5V7h5v2z" />
+                </svg>
+                <div class="">Lorem Ipsum</div>
+            </div>
         </div>
     </div>
 
 
-    {{-- Jendela Status --}}
-    <div
-        class="m-4 grid grid-cols-1 gap-y-2 lg:bg-white lg:shadow-md lg:mx-auto lg:grid-cols-3 lg:p-4 lg:rounded-xl lg:w-[50vw]">
-        <div class="flex gap-2 lg:flex-col">
-            <h2 class="hidden lg:block text-xl font-bold">Lokasi</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" class="lg:hidden h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd"
-                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                    clip-rule="evenodd" />
-            </svg>
-            <div class="">Lorem Ipsum</div>
-        </div>
-        <div class="flex gap-2 lg:flex-col">
-            <h2 class="hidden lg:block text-xl font-bold">Jumlah Kamar</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class="lg:hidden h-5 w-5"
-                fill="currentColor" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 20">
-                <path fill="currentColor"
-                    d="M20 9.557V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.525 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.475-.811-2.75-2-3.443zM18 7v2h-5V7h5zM6 7h5v2H6V7zm14 9H4v-3c0-1.103.897-2 2-2h12c1.103 0 2 .897 2 2v3z" />
-            </svg>
-            <div class="">Lorem Ipsum</div>
-        </div>
-        <div class="flex gap-2 lg:flex-col">
-            <h2 class="hidden lg:block text-xl font-bold">Sisa kamar</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class="lg:hidden h-5 w-5"
-                preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 20">
-                <path fill="currentColor"
-                    d="M20 9.556V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.526 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.474-.811-2.75-2-3.444zM11 9H6V7h5v2zm7 0h-5V7h5v2z" />
-            </svg>
-            <div class="">Lorem Ipsum</div>
-        </div>
-    </div>
 
     {{-- Kategori --}}
-    <div class="prose m-4 lg:mx-auto lg:items-center">
+    <div class="prose m-4 lg:mx-auto lg:items-center lg:m-14">
         <h3 class="lg:text-center">Kategori</h3>
         <div id="kategori-item" class="flex gap-2 lg:justify-center w-full overflow-x-auto">
             <div
@@ -107,7 +110,7 @@ Mimi Kost | Alamat Tempat Kost
 
     @include('components.divider')
 
-    <div id="list" class="gap-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-4 m-4">
+    <div id="list" class="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-4 m-4">
         @for ($i = 1; $i < 8; $i++) <a href="{{ route('tempat-kost-view') }}">
             <div class="card card-compact bg-base-100 shadow-sm" style="min-width: 180px">
                 <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="" />
