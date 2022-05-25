d0cc163ba54c410342d1ca7ae1b7508aae5c6e0c
Firman Wahyudi <e41190305@student.polije.ac.id>
2022-05-25 09:47:29 +0700

    header

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -17,7 +17,7 @@
     <!-- logo -->
     <div class="flex-none w-56 flex flex-row items-center">
       <img src="{{asset('images/logo.png')}}" class="w-10 flex-none">
-      <strong class="capitalize ml-1 flex-1">cleopatra</strong>
+      <strong class="capitalize ml-1 flex-1">Mimi Kost</strong>
 
       <button id="sliderBtn" class="flex-none text-right text-gray-900 hidden md:block">
         <i class="fad fa-list-ul"></i>
