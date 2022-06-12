e7d40fd3dd3944c0d64642db3df3a89e92abbb2e
Firman Wahyudi <e41190305@student.polije.ac.id>
2022-06-12 13:11:38 +0700

    header

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,93 +1,102 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
-  <link rel="shortcut icon" href="./img/fav.png" type="image/x-icon">  
-  <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">  
-  <title>@yield('title')</title>
-  @yield('css-custom')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./img/fav.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <title>@yield('title')</title>
+    @yield('css-custom')
 </head>
+
 <body class="bg-gray-100">
 
 
-<!-- start navbar -->
-<div class="md:fixed md:w-full md:top-0 md:z-20 flex flex-row flex-wrap items-center bg-white p-6 border-b border-gray-300">
-    
-    <!-- logo -->
-    <div class="flex-none w-56 flex flex-row items-center">
-      <img src="{{asset('images/logo.png')}}" class="w-10 flex-none">
-      <strong class="capitalize ml-1 flex-1">Mimi Kost</strong>
+    <!-- start navbar -->
+    <div
+        class="md:fixed md:w-full md:top-0 md:z-20 flex flex-row flex-wrap items-center bg-white p-6 border-b border-gray-300">
 
-      <button id="sliderBtn" class="flex-none text-right text-gray-900 hidden md:block">
-        <i class="fad fa-list-ul"></i>
-      </button>
-    </div>
-    <!-- end logo -->   
-    
-    <!-- navbar content toggle -->
-    <button id="navbarToggle" class="hidden md:block md:fixed right-0 mr-6">
-      <i class="fad fa-chevron-double-down"></i>
-    </button>
-    <!-- end navbar content toggle -->
+        <!-- logo -->
+        <div class="flex-none w-56 flex flex-row items-center">
+            <img src="{{ asset('images/logo.png') }}" class="w-10 flex-none">
+            <strong class="capitalize ml-1 flex-1">Mimi Kost</strong>
 
-    <!-- navbar content -->
-    <div id="navbar" class="animated md:hidden md:fixed md:top-0 md:w-full md:left-0 md:mt-16 md:border-t md:border-b md:border-gray-200 md:p-10 md:bg-white flex-1 pl-3 flex flex-row flex-wrap justify-between items-center md:flex-col md:items-center">
-      <!-- left -->
-      <div class="text-gray-600 md:w-full md:flex md:flex-row md:justify-evenly md:pb-10 md:mb-10 md:border-b md:border-gray-200">
-                
-      </div>
-      <!-- end left -->      
+            <button id="sliderBtn" class="flex-none text-right text-gray-900 hidden md:block">
+                <i class="fad fa-list-ul"></i>
+            </button>
+        </div>
+        <!-- end logo -->
 
-      <!-- right -->
-      <div class="flex flex-row-reverse items-center"> 
+        <!-- navbar content toggle -->
+        <button id="navbarToggle" class="hidden md:block md:fixed right-0 mr-6">
+            <i class="fad fa-chevron-double-down"></i>
+        </button>
+        <!-- end navbar content toggle -->
 
-        <!-- user -->
-        <div class="dropdown relative md:static">
+        <!-- navbar content -->
+        <div id="navbar"
+            class="animated md:hidden md:fixed md:top-0 md:w-full md:left-0 md:mt-16 md:border-t md:border-b md:border-gray-200 md:p-10 md:bg-white flex-1 pl-3 flex flex-row flex-wrap justify-between items-center md:flex-col md:items-center">
+            <!-- left -->
+            <div
+                class="text-gray-600 md:w-full md:flex md:flex-row md:justify-evenly md:pb-10 md:mb-10 md:border-b md:border-gray-200">
 
-          <button class="menu-btn focus:outline-none focus:shadow-outline flex flex-wrap items-center">
-            <div class="w-8 h-8 overflow-hidden rounded-full">
-              <img class="w-full h-full object-cover" src="{{asset('images/user.svg')}}" >
-            </div> 
+            </div>
+            <!-- end left -->
 
-            <div class="ml-2 capitalize flex ">
-              <h1 class="text-sm text-gray-800 font-semibold m-0 p-0 leading-none">moeSaid</h1>
-              <i class="fad fa-chevron-down ml-2 text-xs leading-none"></i>
-            </div>                        
-          </button>
+            <!-- right -->
+            <div class="flex flex-row-reverse items-center">
 
-          <button class="hidden fixed top-0 left-0 z-10 w-full h-full menu-overflow"></button>
+                <!-- user -->
+                <div class="dropdown relative md:static">
 
-          <div class="text-gray-500 menu hidden md:mt-10 md:w-full rounded bg-white shadow-md absolute z-20 right-0 w-40 mt-5 py-2 animated faster">
+                    <button class="menu-btn focus:outline-none focus:shadow-outline flex flex-wrap items-center">
+                        <div class="w-8 h-8 overflow-hidden rounded-full">
+                            <img class="w-full h-full object-cover" src="{{ asset('images/user.svg') }}">
+                        </div>
 
-            <!-- item -->
-            <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out" href="#">
-              <i class="fad fa-user-edit text-xs mr-1"></i> 
-              edit my profile
-            </a>     
-            <!-- end item -->
-            <hr>
+                        <div class="ml-2 capitalize flex ">
+                            <h1 class="text-sm text-gray-800 font-semibold m-0 p-0 leading-none">
+                                {{ Auth()->user()->name }}</h1>
+                            <i class="fad fa-chevron-down ml-2 text-xs leading-none"></i>
+                        </div>
+                    </button>
 
-            <!-- item -->
-            <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    <button class="hidden fixed top-0 left-0 z-10 w-full h-full menu-overflow"></button>
+
+                    <div
+                        class="text-gray-500 menu hidden md:mt-10 md:w-full rounded bg-white shadow-md absolute z-20 right-0 w-40 mt-5 py-2 animated faster">
+
+                        {{-- <!-- item -->
+                        <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out"
+                            href="#">
+                            <i class="fad fa-user-edit text-xs mr-1"></i>
+                            edit my profile
+                        </a>
+                        <!-- end item -->
+                        <hr> --}}
+
+                        <!-- item -->
+                        <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out"
+                            href="{{ route('logout') }}" onclick="event.preventDefault();
               document.getElementById('logout-form').submit();">
 
-              <i class="fad fa-user-times text-xs mr-1"></i> 
-              {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-              @csrf
-            </form>     
-            <!-- end item -->
+                            <i class="fad fa-user-times text-xs mr-1"></i>
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        <!-- end item -->
 
-          </div>
+                    </div>
+                </div>
+                <!-- end user -->
+            </div>
+            <!-- end right -->
         </div>
-        <!-- end user -->
-      </div>
-      <!-- end right -->
-    </div>
-    <!-- end navbar content -->
+        <!-- end navbar content -->
 
-  </div>
-<!-- end navbar -->
+    </div>
+    <!-- end navbar -->
